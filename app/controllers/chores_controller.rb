699e976ab6ddf1d7b70f89ae3e48df31f0e34530
Alex Emrie <emrieaj@gmail.com>
2015-02-16 14:12:23 -0700

    Completed Crud Activity

---
+++ app/controllers/chores_controller.rb
@@ -0,0 +1,50 @@
+class ChoresController < ApplicationController
+  def index
+    @chores = Chore.all
+  end
+
+  def new
+    @chore = Chore.new
+  end
+
+  def create
+    chore = Chore.new(chore_params)
+    if chore.save
+      redirect_to chores_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @chore = Chore.find(params[:id])
+  end
+
+  def update
+    chore= Chore.find(params[:id])
+
+    if chore.update(chore_params)
+      redirect_to chore_path(chore)
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @chore = Chore.find(params[:id])
+  end
+
+  def destroy
+    chore = Chore.find(params[:id])
+
+    chore.destroy
+      redirect_to chores_path
+  end
+
+
+  def chore_params
+    params.require(:chore).permit(:tasks, :due_date)
+  end
+
+
+end
